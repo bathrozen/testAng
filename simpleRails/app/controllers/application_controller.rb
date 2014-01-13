2ffc7f78d60ccc58ff030c8f3289616306a253c3
Warm <warm@intropica,com>
2014-01-13 15:22:42 +0700

    move to authen by fb

--- simpleRails/app/controllers/application_controller.rb
+++ simpleRails/app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
+
+  before_filter :fbAuthen
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def fbAuthen
+    render :file => File.join(Rails.root, 'public', 'auth.html'), :layout => nil
+  end
+
 end
