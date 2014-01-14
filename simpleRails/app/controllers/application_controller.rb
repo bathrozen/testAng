57da4cd0f8d63941884f319bef8c053aea3b9c30
Warm <warm@intropica,com>
2014-01-14 14:20:52 +0700

    try to stop infinite loop

--- simpleRails/app/controllers/application_controller.rb
+++ simpleRails/app/controllers/application_controller.rb
@@ -2,17 +2,23 @@ class ApplicationController < ActionController::Base
 
   # before_filter :authenticate_user!
 
-  before_filter :fbAuthen
+  before_filter :findUser
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def fbAuthen
-    if !params['fID']
+  def fbAuthen(user)
+    if user
       render :file => File.join(Rails.root, 'public', 'auth.html'), :layout => nil
+    end
+  end
+
+  def findUser
+    if !(user = User.find_by_fID(params['fID']))
+      User.create_by_fID(params['fID']);
     else
-      render :file => File.join(Rails.root, 'public_2', 'index.html'), :layout => nil
+      user
     end
   end
 
