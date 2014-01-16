f47880f9e0968757027f7f5504a08330bdead5b6
Warm <warm@intropica,com>
2014-01-16 14:55:53 +0700

    send userID to server

--- simpleRails/manager/app/js/utility/phone_resource.js
+++ simpleRails/manager/app/js/utility/phone_resource.js
@@ -1,24 +1,25 @@
 angular.module('phonecatApp')
 
-.factory('phoneResource', function($http, angularSocket){
-	var phoneRes = {};
+.factory('phoneResource', function($http, angularSocket, $rootScope){
+	var phoneRes = {},
+		userID = $rootScope.userData.id;
 
 	phoneRes.get = function(){
 		return $http.get('/api/phones');
 	};
 
 	phoneRes.new = function(phone){
-		var data = {phone: phone, sessionID: angularSocket.socket.sessionid};
+		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, userID: userID};
 		return $http.post('/api/phones', data);
 	};
 
 	phoneRes.update = function(phone){
-		var data = {phone: phone, sessionID: angularSocket.socket.sessionid};
+		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, userID: userID};
 		return $http.put('/api/phones/'+phone.id, data);
 	};
 
 	phoneRes.delete = function(phone){
-		return $http.delete('/api/phones/'+phone.id+'?sessionID='+angularSocket.socket.sessionid);
+		return $http.delete('/api/phones/'+phone.id+'?sessionID='+angularSocket.socket.sessionid+'&userID='+userID);
 	};
 
 	return phoneRes;
