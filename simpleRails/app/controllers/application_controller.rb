6a04c99aaa78def66bcdf56af62964fe584b9109
Warm <warm@intropica,com>
2014-01-17 15:57:16 +0700

    no, try sign_in user again

--- simpleRails/app/controllers/application_controller.rb
+++ simpleRails/app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :facebook_authen
 
-  cattr_accessor :user
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,13 +14,10 @@ class ApplicationController < ActionController::Base
       else
         p 'create_by_fbData'
         data = {:name => params['name'], :fID => params['fID']}
-        @user = User.create_from_fbData(data);
+        user = User.create_from_fbData(data);
+        sign_in user
       end
     end
-  end
-
-  def user
-    @user
   end
 
 end
