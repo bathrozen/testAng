a52c314e65ac8f0175587d920505cf249758093c
Warm <warm@intropica,com>
2014-01-17 17:27:45 +0700

    update phone to save user too

--- simpleRails/app/controllers/application_controller.rb
+++ simpleRails/app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :facebook_authen unless :signed_in?
 
-  # skip_before_filter :facebook_authen if sign_in?
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- simpleRails/app/models/phone.rb
+++ simpleRails/app/models/phone.rb
@@ -2,8 +2,8 @@ class Phone < ActiveRecord::Base
     has_many :details, :dependent => :destroy
     belongs_to :user
 
-    def self.addPhone(phoneData)
-      phoneObj = Phone.create({:name => phoneData['name']})
+    def self.addPhone(phoneData, user)
+      phoneObj = Phone.create(:name => phoneData['name'], :user => user)
       if !phoneData['snippet'].nil?
         Detail.create({:snippet => phoneData['snippet'], :phone => phoneObj})
       end
