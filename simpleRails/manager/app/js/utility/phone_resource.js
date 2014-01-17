9238852c85b043753b782d030ebd2d032ce7a7b9
Warm <warm@intropica,com>
2014-01-17 11:19:21 +0700

    update user_id to facebookID

--- simpleRails/manager/app/js/utility/phone_resource.js
+++ simpleRails/manager/app/js/utility/phone_resource.js
@@ -8,17 +8,17 @@ angular.module('phonecatApp')
 	};
 
 	phoneRes.new = function(phone, user){
-		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, userID: currentUser.id};
+		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, facebookID: currentUser.id};
 		return $http.post('/api/phones', data);
 	};
 
 	phoneRes.update = function(phone, user){
-		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, userID: currentUser.id};
+		var data = {phone: phone, sessionID: angularSocket.socket.sessionid, facebookID: currentUser.id};
 		return $http.put('/api/phones/'+phone.id, data);
 	};
 
 	phoneRes.delete = function(phone, user){
-		return $http.delete('/api/phones/'+phone.id+'?sessionID='+angularSocket.socket.sessionid+'&userID='+currentUser.id);
+		return $http.delete('/api/phones/'+phone.id+'?sessionID='+angularSocket.socket.sessionid+'&facebookID='+currentUser.id);
 	};
 
 	return phoneRes;
