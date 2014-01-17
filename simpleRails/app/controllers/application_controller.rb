b863171190e045de767fd2b0aa1ee5c283fce079
Warm <warm@intropica,com>
2014-01-17 15:00:15 +0700

    Checkpoint create new id by facebook or use exist one

--- simpleRails/app/controllers/application_controller.rb
+++ simpleRails/app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  # before_filter :authenticate_user!
-
   before_filter :facebook_authen
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-#   def after_authen
-# #    sign_in @user
-#     render :file => File.join(Rails.root, 'public_2', 'index.html'), :layout => nil
-#   end
 
   def facebook_authen
     if !params['fID'].nil?
