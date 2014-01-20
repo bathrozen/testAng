546de54fb2cb8c05400a3164911de6b3061c79ad
Warm <warm@intropica,com>
2014-01-20 17:11:02 +0700

    add .facebookID to Phone3

--- simpleRails/app/models/persistent_phone.rb
+++ simpleRails/app/models/persistent_phone.rb
@@ -1,6 +1,6 @@
 class PersistentPhone < ActiveRecord::Base
     has_many :details, :dependent => :destroy
-    has_one :facebookID, :through => :user
+    has_one :user, :through => :facebookID
     belongs_to :user
   self.table_name = 'phones'
 
