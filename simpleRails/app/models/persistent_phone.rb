8ce5ec56a7c8b8939217a10c936e6a7bb7d7fe99
Warm <warm@intropica,com>
2014-01-20 09:05:01 +0700

    more files

---
+++ simpleRails/app/models/persistent_phone.rb
@@ -0,0 +1,14 @@
+class PersistentPhone < ActiveRecord::Base
+    has_many :details, :dependent => :destroy
+    belongs_to :user
+  self.table_name = 'phones'
+
+    def self.addPhone(phoneData, user)
+      phoneObj = PersistentPhone.create(:name => phoneData['name'], :user => user)
+      if !phoneData['snippet'].nil?
+        Detail.create({:snippet => phoneData['snippet'], :phone => phoneObj})
+      end
+      phoneObj
+    end
+
+end
